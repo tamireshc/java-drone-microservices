56208d3f37d3511145925bb68bb5ac2ed520e720
tamireshc <tamireshc@gmail.com>
2024-10-29 09:12:46 -0300

    feat: add conf at application.properties ms-gerenciador-cadastros

--- ms-gerenciador-pedidos/src/main/java/com/example/ms_gerenciador_pedidos/MsGerenciadorPedidosApplication.java
+++ ms-gerenciador-pedidos/src/main/java/com/example/ms_gerenciador_pedidos/MsGerenciadorPedidosApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_gerenciador_pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsGerenciadorPedidosApplication {
 
 	public static void main(String[] args) {
