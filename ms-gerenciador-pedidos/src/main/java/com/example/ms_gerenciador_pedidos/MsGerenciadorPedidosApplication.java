941e89b2a57596f5457b0aecae8e721e2bd3643f
tamireshc <tamireshc@gmail.com>
2024-10-29 09:06:12 -0300

    feat: add ms-gerenciador-pedidos

---
+++ ms-gerenciador-pedidos/src/main/java/com/example/ms_gerenciador_pedidos/MsGerenciadorPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_gerenciador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGerenciadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGerenciadorPedidosApplication.class, args);
+	}
+
+}
