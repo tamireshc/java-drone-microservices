49b13320b4e71d2185fef8762dcb71b1f3f2bc11
tamireshc <tamireshc@gmail.com>
2024-11-26 11:00:48 -0300

    feat: add MonitoramentoService

---
+++ ms-monitoramento/src/main/java/com/example/ms_monitoramento/service/MonitoramentoService.java
@@ -0,0 +1,22 @@
+package com.example.ms_monitoramento.service;
+
+import com.example.ms_monitoramento.model.DronePedidoId;
+import com.example.ms_monitoramento.model.Monitoramento;
+import com.example.ms_monitoramento.repository.MonitoramentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MonitoramentoService {
+    @Autowired
+    private MonitoramentoRepository monitoramentoRepository;
+
+    public Monitoramento buscarMonitoramentoPorId(String droneId, String pedidoId) {
+        DronePedidoId id = new DronePedidoId(Long.parseLong(droneId), Long.parseLong(pedidoId));
+        Monitoramento monitoramento = monitoramentoRepository.findById(id).orElse(null);
+        if (monitoramento == null) {
+            return null;
+        }
+        return monitoramento;
+    }
+}
