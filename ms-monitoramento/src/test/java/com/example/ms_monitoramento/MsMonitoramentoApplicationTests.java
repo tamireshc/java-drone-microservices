00fd72bf1f41c57376467bcadbc365d1f5acde65
tamireshc <tamireshc@gmail.com>
2024-11-26 09:30:06 -0300

    feat: add ms-monitoramento

---
+++ ms-monitoramento/src/test/java/com/example/ms_monitoramento/MsMonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
