bf26f5c2d2184f72d4cf06edf0ea40ab0c9b4b30
tamireshc <tamireshc@gmail.com>
2024-12-02 10:30:38 -0300

    feat: add DadosPedidoDTO in ms-notificador

--- ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/constants/MensagemConstante.java
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/constants/MensagemConstante.java
@@ -1,7 +1,8 @@
 package com.example.ms_notificador.constants;
 
 public class MensagemConstante {
-    public static final String PEDIDO_CRIADO = "Prezado(a) %s, o pedido %d foi criado com sucesso";
-    public static final String PEDIDO_EM_ROTA = "Prezado(a) %s, o pedido %d saiu para a entrega";
-    public static final String PEDIDO_CANCELADO = "Prezado(a) %s, o pedido %d foi cancelado";
+    public static final String PEDIDO_CRIADO = "Prezado(a) %s, o pedido %d realizado em %s foi criado com sucesso";
+    public static final String PEDIDO_EM_ROTA = "Prezado(a) %s, o pedido %d realizado em %s saiu para a entrega";
+    public static final String PEDIDO_ENTREGUE = "Prezado(a) %s, o pedido %d realizado em %s foi entregue em %s";
+    public static final String PEDIDO_CANCELADO = "Prezado(a) %s, o pedido %d realizado em %s foi cancelado";
 }
---
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/dto/DadosPedidoDTO.java
@@ -0,0 +1,16 @@
+package com.example.ms_notificador.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DadosPedidoDTO {
+    private Long pedidoId;
+    private String nome;
+    private String telefone;
+    private LocalDateTime dataPedido;
+    private LocalDateTime dataEntrega;
+}
