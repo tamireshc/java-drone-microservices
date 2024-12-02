1ff4ecab53cb9f2e4c12ea4ec5e7c37f06d58465
tamireshc <tamireshc@gmail.com>
2024-12-02 10:14:00 -0300

    feat: add NotificacaoSNSService in ms-notificador

---
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/service/NotificacaoSNSService.java
@@ -0,0 +1,18 @@
+package com.example.ms_notificador.service;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacaoSNSService {
+    @Autowired
+    private AmazonSNS amazonSNS;
+
+    public void notificar(String telefone, String message) {
+        PublishRequest publishRequest = new PublishRequest().withMessage(message)
+                .withPhoneNumber(telefone);
+        amazonSNS.publish(publishRequest);
+    }
+}
