339a8554a254a9eef2926db0d221475dbe676a42
tamireshc <tamireshc@gmail.com>
2024-12-02 09:56:23 -0300

    feat: add msg constants in ms-notificador

---
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/config/AmazonSnsConfiguracao.java
@@ -0,0 +1,32 @@
+package com.example.ms_notificador.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+
+@Configuration
+public class AmazonSnsConfiguracao {
+    // Configuração do Amazon SNS
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+    private final String awsAccessKey = dotenv.get("AWS_ACCESS_KEY");
+    private final String awsSecretKey = dotenv.get("AWS_SECRET_KEY");
+
+    @Bean
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+        return AmazonSNSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
+                .withRegion(Regions.US_EAST_1).build();
+    }
+}
---
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/constants/MensagemConstante.java
@@ -0,0 +1,7 @@
+package com.example.ms_notificador.constants;
+
+public class MensagemConstante {
+    public static final String PEDIDO_CRIADO = "Prezado(a) %s, o pedido %d foi criado com sucesso";
+    public static final String PEDIDO_EM_ROTA = "Prezado(a) %s, o pedido %d saiu para a entrega";
+    public static final String PEDIDO_CANCELADO = "Prezado(a) %s, o pedido %d foi cancelado";
+}
