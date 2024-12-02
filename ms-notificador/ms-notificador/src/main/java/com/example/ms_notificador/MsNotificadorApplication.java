57810d3a4a30dbb38f0457c176c497fd4b823a3e
tamireshc <tamireshc@gmail.com>
2024-12-02 08:43:35 -0300

    feat: add ms-notificador

---
+++ ms-notificador/ms-notificador/src/main/java/com/example/ms_notificador/MsNotificadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_notificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificadorApplication.class, args);
+	}
+
+}
